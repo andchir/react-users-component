4296f4a4bef432269e44d64a65b0cf6f4a576ea5
andchir <andchir@gmail.com>
2019-04-17 15:57:37 +0300

    Update filters, add date picker

--- src/App.js
+++ src/App.js
@@ -1,77 +1,122 @@
 import React from 'react';
 import axios from 'axios';
+import PropTypes from "prop-types";
+import DatePicker, { registerLocale } from "react-datepicker";
+import ru from 'date-fns/locale/ru';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-datepicker/dist/react-datepicker.css";
 import './styles.css';
 
-function SearchForm() {
-    return (
-        <div className="card card-body bg-light mb-3">
+registerLocale('ru', ru);
 
-            <div className="row">
-                <div className="col-lg-5">
+class CustomCalendarInput extends React.Component {
+    static propTypes = {
+        onClick: PropTypes.func,
+        value: PropTypes.string,
+        placeholderText: PropTypes.string
+    };
+    render () {
+        console.log(this.props);
+        return (
+            <button
+                className="btn btn-info btn-block"
+                onClick={this.props.onClick}>
+                {this.props.placeholderText}: {this.props.value ? this.props.value : 'Нет даты'}
+            </button>
+        )
+    }
+}
 
-                    <div className="input-group mb-3">
-                        <div className="input-group-prepend input-group-prepend-wide">
+class SearchForm extends React.Component {
+    static propTypes = {
+        onChange: PropTypes.func,
+        filters: PropTypes.shape({
+            lastName: PropTypes.string,
+            city: PropTypes.string,
+            phone: PropTypes.string,
+            dateOfBirth: PropTypes.instanceOf(Date)
+        })
+    };
+    render() {
+        return (
+            <div className="card card-body bg-light mb-3">
+
+                <div className="row">
+                    <div className="col-lg-5">
+
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend input-group-prepend-wide">
                             <span className="input-group-text">
                                 <span>Фамилия</span>
                             </span>
+                            </div>
+                            <input type="text" className="form-control" onChange={(e) => this.props.onChange('lastName', e.target.value)} />
                         </div>
-                        <input type="text" className="form-control" />
+
                     </div>
+                    <div className="col-lg-5">
 
-                </div>
-                <div className="col-lg-5">
-
-                    <div className="input-group mb-3">
-                        <div className="input-group-prepend input-group-prepend-wide">
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend input-group-prepend-wide">
                             <span className="input-group-text">
                                 <span>
                                     Телефон
                                 </span>
                             </span>
+                            </div>
+                            <input type="text" className="form-control" onChange={(e) => this.props.onChange('phone', e.target.value)} />
                         </div>
-                        <input type="text" className="form-control" />
+
                     </div>
-
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-lg-5">
+                <div className="row">
+                    <div className="col-lg-5">
 
-                    <div className="input-group mb-3 mb-lg-0">
-                        <div className="input-group-prepend input-group-prepend-wide">
+                        <div className="input-group mb-3 mb-lg-0">
+                            <div className="input-group-prepend input-group-prepend-wide">
                             <span className="input-group-text">
                                 <span>
                                     Город
                                 </span>
                             </span>
+                            </div>
+                            <input type="text" className="form-control" onChange={(e) => this.props.onChange('city', e.target.value)} />
                         </div>
-                        <input type="text" className="form-control" />
+
                     </div>
+                    <div className="col-lg-5">
 
-                </div>
-                <div className="col-lg-5">
-
-                    <div className="input-group mb-3 mb-lg-0">
-                        <div className="input-group-prepend input-group-prepend-wide">
-                            <span className="input-group-text">
-                                <span>
-                                    Дата рождения
+                        <div className="input-group mb-3 mb-lg-0">
+                            <div className="input-group-prepend input-group-prepend-wide">
+                                <span className="input-group-text">
+                                    <span>
+                                        Дата рождения
+                                    </span>
                                 </span>
-                            </span>
+                            </div>
+                            <DatePicker
+                                className="form-control"
+                                selected={this.props.filters.dateOfBirth}
+                                onChange={(date) => this.props.onChange('dateOfBirth', date)}
+                                dateFormat="dd/MM/yyyy"
+                                locale="ru"
+                                placeholderText="Выбрать дату"
+                                isClearable={true}
+                                showMonthDropdown
+                                showYearDropdown />
                         </div>
+
+                    </div>
+                    <div className="col-lg-2">
+                        <button type="button" className="btn btn-info btn-block">
+                            Сброс
+                        </button>
                     </div>
 
                 </div>
-                <div className="col-lg-2">
-                    <button type="button" className="btn btn-info btn-block">
-                        Сброс
-                    </button>
-                </div>
-
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 class UserRow extends React.Component {
@@ -141,11 +186,17 @@ class UsersTable extends React.Component {
 }
 
 class App extends React.Component {
-
     constructor(props) {
         super(props);
+        this.timer = null;
         this.state = {
-            users: []
+            users: [],
+            filters: {
+                lastName: '',
+                city: '',
+                phone: '',
+                dateOfBirth: null
+            }
         };
     }
 
@@ -162,12 +213,25 @@ class App extends React.Component {
             });
     }
 
+    onFilterChange = (name, value) => {
+        clearTimeout(this.timer);
+        this.timer = setTimeout(() => {
+            console.log('onFilterChange', name, value);
+            const filters = Object.assign({}, this.state.filters, {[name]: value});
+            this.setState({
+                filters: filters
+            });
+        }, 400);
+    };
+
     render() {
         return (
             <div className="App py-3">
                 <div className="container">
 
-                    <SearchForm />
+                    <SearchForm
+                        filters={this.state.filters}
+                        onChange={this.onFilterChange.bind(this)} />
 
                     <UsersTable users={this.state.users} />
 
